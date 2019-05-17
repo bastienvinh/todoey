041e8020a21256e00f3e31ce2d82ff1702941089
Bastien VINH <bvinh.dev@yahoo.com>
2019-05-17 12:14:26 +0200

    Initial Commit

---
+++ Todaey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todaey
+//
+//  Created by Cloud Strife on 5/17/19.
+//  Copyright © 2019 Strife Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
